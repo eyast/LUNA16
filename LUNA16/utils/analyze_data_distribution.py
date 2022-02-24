d9de7945e52485beee674f4df25a1b04dca4cfdd
Eyas <eyast@microsoft.com>
2022-02-24 05:52:23 +0000

    EDA

--- LUNA16/utils/analyze_data_distribution.py
+++ LUNA16/utils/analyze_data_distribution.py
@@ -1,33 +1,32 @@
+from collections import namedtuple
 from functools import lru_cache
-import time
 from typing import Counter, List
 
 import numpy as np
 import SimpleITK as sitk
-from tqdm import tqdm
+
 
 @lru_cache(maxsize=100, typed=True)
-def analyze_shapes(list_of_files: List) -> np.array:
+def analyze_shapes(list_of_files: List, type: int) -> int:
     """Returns a list of all file shapes
-    
+
     Arguments:
     - list_of_files: a Listof files with the MHD extension.
-    
+
     Returns:
-    - numpy array that describes the sizes and their counts"""
-    returns = []
+    - TBD
+    """
     for file in list_of_files:
+        File_analysis = namedtuple(
+            "File_analysis",
+            ["num_channels", "size", "ch1", "ch2", "ch3"])
         # Read each file
         data = sitk.ReadImage(file.folder)
         data = np.array(sitk.GetArrayFromImage(data), dtype=np.float32)
         # Ensure that all files have the same number of channels
-        returns.append(len(data.shape))
-        #data = np.reshape(data, -1)
-        #data = data.shape[0]
-        #results.append(data)
-    return returns
-
-
+        ops = [len(data.shape), data.size, data.shape[0], data.shape[1],
+            data.shape[2]]
+    return ops[type]
 
 
 def analyze_data_distribution(list_of_files: List, bins: int = 100) -> np.array:
@@ -55,4 +54,3 @@ def analyze_data_distribution(list_of_files: List, bins: int = 100) -> np.array:
 def find_nans(list_of_files: List) -> Counter:
     """Returns the count and location of NaNs"""
     ...
-
