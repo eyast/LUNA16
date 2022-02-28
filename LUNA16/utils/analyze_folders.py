73411467a22ce2c5cf5e5e09f080f0ff58535046
eyas taifour <eyast@microsoft.com>
2022-02-28 23:30:18 +0000

    Rendered nb 01

--- LUNA16/utils/analyze_folders.py
+++ LUNA16/utils/analyze_folders.py
@@ -3,7 +3,7 @@ from collections import namedtuple
 from typing import List
 
 
-File = namedtuple('File', ['filename', 'folder', 'extension', 'size'])
+File = namedtuple('File', ['filename', 'folder', 'extension', 'size', 'path'])
 
 
 def analyze_folder(ROOT_FOLDER: str) -> List[File]:
@@ -16,7 +16,7 @@ def analyze_folder(ROOT_FOLDER: str) -> List[File]:
     - all_files: A list of File namedtuple objects.
 
     Each file has the following signature:
-    File = namedtuple('File', ['filename', 'folder', 'extension', 'size'])
+    File = namedtuple('File', ['filename', 'folder', 'extension', 'size', 'path'])
     """
     all_files = []
     for path, dirs, files in os.walk(ROOT_FOLDER):
@@ -25,7 +25,7 @@ def analyze_folder(ROOT_FOLDER: str) -> List[File]:
             extension = fp.split(".")[-1]
             size = os.path.getsize(fp)
             f = f[:-4]
-            individual_file = File(f, fp, extension, size)
+            individual_file = File(f, fp, extension, size, path)
             all_files.append(individual_file)
     return all_files
 
