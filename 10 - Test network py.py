19d88a15633c0a5678a62cc8aeb175baadf9ef35
eyas taifour <eyast@microsoft.com>
2022-03-04 00:51:36 +0000

    xyz network built

--- 10 - Test network py.py
+++ 10 - Test network py.py
@@ -4,21 +4,32 @@ import torch
 from torch.utils.data import DataLoader
 from torch.optim import Adam
 from torch.nn import BCELoss
-from sklearn.metrics import accuracy_score, precision_score, recall_score
-ds = LUNAdatastore_position()
-dl = DataLoader(ds, 4)
-val = DataLoader(ds, 4)
+import wandb
+
+wandb.init(project="LUNA_xyz")
+
+train_size = 100000
+val_size = 7500
+epochs = 200
+ds = LUNAdatastore_position(len=train_size)
+
+vs = LUNAdatastore_position(len=val_size)
+dl = DataLoader(ds, 4, shuffle=True)
+val = DataLoader(ds, 4, shuffle=True)
 
 model_test = xyz_net2().to('cuda')
 opt = Adam(model_test.parameters(), lr=1e-3)
 criterion = BCELoss()
 
-for i in range(20):
+for i in range(epochs):
     epoch_loss = 0
-    eval_loss = 0
+    val_loss = 0
     count = 0
     eval_count = 0
     correct = 0
+    val_correct = 0
+    accuracy = 0
+    val_accuracy = 0
     torch.set_grad_enabled(True)
     for j, mini_batch in enumerate(dl):
         opt.zero_grad()
@@ -32,8 +43,27 @@ for i in range(20):
         loss.backward()
         opt.step()
         preds_thr = (preds > 0.5).float()
-        accuracy = precision_score(label.cpu().numpy(), preds_thr.cpu().numpy())
-        print(accuracy)
-        #correct += (preds == label).float().sum()
+        correct += (preds_thr == label).float().sum()
+    epoch_loss = epoch_loss / count
     accuracy = 100 * correct / len(ds)
-    print (f"{i}:\t{j}:\t{epoch_loss/count}:\t{accuracy}")
+    torch.set_grad_enabled(False)
+    for k, mini_batch in enumerate(val):
+        model_test.eval()
+        data = mini_batch["data"].to('cuda')
+        label = mini_batch["label"].to('cuda')
+        preds = model_test(data)
+        loss = criterion(preds, label).mean()
+        val_loss += loss
+        eval_count += 1
+        preds_thr = (preds > 0.5).float()
+        val_correct += (preds_thr == label).float().sum()
+    val_loss = val_loss / eval_count
+    val_accuracy = 100 * val_correct / len(vs)
+    wandb.log(data={
+        't_loss': epoch_loss,
+        'v_loss': val_loss,
+        't_acc': accuracy,
+        'v_acc': val_accuracy
+    })
+    print (f"{i}:\t t_loss: {epoch_loss}:\t \
+                t_acc: {accuracy}:\t val_loss: {val_loss}:\t val_acc: {val_accuracy}")
--- LUNA16/utils/LUNAdatastore.py
+++ LUNA16/utils/LUNAdatastore.py
@@ -52,42 +52,103 @@ class LUNAdatastore(Dataset):
 
 
 class LUNAdatastore_position(LUNAdatastore):
-    def __init__(self, len=50000, dist=5):
+    """ Generates a Pytorch dataset based on location of nodules.
+    
+    Arguments:
+    - len: Length of the data.
+    - dist: minimum distance in mm to consider two nodules to be neighbors.
+    - pos_percent: percentage of pos/ values."""
+    def __init__(self, len=50000, dist=5, pos_percent=0.5):
         super().__init__()
         self.len = len
         self.dist = dist
         self.x = [-300, 300]
         self.y = [-300, 300]
         self.z = [-500, 500]
+        self.pos_percent = pos_percent
         for i, row in self.annotations.iterrows():
             self.annotations.loc[i, "point"] = Point(
                 row["coordX"], row["coordY"], row["coordZ"])
+        self.negatives, self.positives = [], []
+        self.results = {}
+        self._populate()
+
+    def _populate(self, ):
+        while len(self.positives) < round(self.len * self.pos_percent):
+            self.positives.append(self.gen_pos_point())
+        while len(self.negatives) < round(self.len * (1 - self.pos_percent)):
+            self.negatives.append(self.gen_neg_point())
+
+
+    def gen_pos_point(self):
+        df_idx = self.annotations.shape[0]
+        r_idx = random.choice(range(0, df_idx))
+        r_idx = self.annotations.loc[r_idx, "point"]
+        randx = random.uniform(-self.dist, self.dist)
+        randy = random.uniform(-self.dist, self.dist)
+        randz = random.uniform(-self.dist, self.dist)
+        new_point = Point(
+            r_idx.x + randx,
+            r_idx.y + randy,
+            r_idx.z + randz
+        )
+        return new_point
+
+
+    def gen_neg_point(self):
+        x = random.uniform(*self.x)
+        y = random.uniform(*self.y)
+        z = random.uniform(*self.z)
+        data = np.array([x, y, z])
+        temp_point = Point(x, y, z)
+        for i, row in self.annotations.iterrows():
+            point = row["point"]
+            if not temp_point.is_neighbor(point, self.dist):
+                return temp_point
 
     def __len__(self):
         return self.len
 
 
     def __getitem__(self, index):
-        random.seed(index)
         result = {}
-
-        # Generates a random point
-        x = random.uniform(*self.x)
-        y = random.uniform(*self.y)
-        z = random.uniform(*self.z)
-        data = np.array([x, y, z])
+        if index > round(self.len * self.pos_percent):
+            index = int(index - round(self.len * self.pos_percent))
+            data = self.negatives[index]
+            yval = 0
+        else:
+            data = self.positives[index-1]
+            yval = 1
+        data = np.array([data.x, data.y, data.z])
         data = torch.from_numpy(data).float()
-        temp_point = Point(x, y, z)
         result["data"] = data
-
-        # Find if it's a nodule or not
-        all_points = self.annotations.point
-        for point in all_points:
-            if temp_point.is_neighbor(point, self.dist):
-                label = np.array(1)
-            else:
-                label = np.array(0)
+        label = np.array(yval)
         label = torch.from_numpy(label).float()
         label = torch.unsqueeze(label, dim=0)
         result["label"] = label
         return result
+
+    # def __getitem__(self, index):
+    #     random.seed(index)
+    #     result = {}
+
+    #     # Generates a random point
+    #     x = random.uniform(*self.x)
+    #     y = random.uniform(*self.y)
+    #     z = random.uniform(*self.z)
+    #     data = np.array([x, y, z])
+    #     data = torch.from_numpy(data).float()
+    #     temp_point = Point(x, y, z)
+    #     result["data"] = data
+
+    #     # Find if it's a nodule or not
+    #     all_points = self.annotations.point
+    #     for point in all_points:
+    #         if temp_point.is_neighbor(point, self.dist):
+    #             label = np.array(1)
+    #         else:
+    #             label = np.array(0)
+    #     label = torch.from_numpy(label).float()
+    #     label = torch.unsqueeze(label, dim=0)
+    #     result["label"] = label
+    #     return result
